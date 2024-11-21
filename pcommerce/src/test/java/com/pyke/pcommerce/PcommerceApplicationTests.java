cde6a065c4e9975dc80d2dd637f581a2ad113a09
Paul Christian <paulchristianmasayon2020@gmail.com>
2024-11-22 02:54:39 +0800

    used Spring initializer to generate project starting with some dependencies

---
+++ pcommerce/src/test/java/com/pyke/pcommerce/PcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyke.pcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
